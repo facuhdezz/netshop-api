c5951a9ddb21ce659df4f9f3fe9fdf7e6f7476fa
FacuHdezz <facuhernandez2000@gmail.com>
2024-02-28 00:53:46 -0300

    agregando productos y vinculando con api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   // Otras configuraciones de Astro...
   server: {
-    routes: [
-      {
-        match: '/api/*',
-        serve: () => ({
-          // Cambia 'http://localhost:3000' al host y puerto de tu servidor Node.js
-          html: `<script type="module" src="http://localhost:3000${
-            new URL(request.url).pathname
-          }"></script>`
-        })
-      }
-    ]
-  }
-}); 
+    routes: [{
+      match: '/api/*',
+      serve: () => ({
+        // Cambia 'http://localhost:3000' al host y puerto de tu servidor Node.js
+        html: `<script type="module" src="http://localhost:3000${new URL(request.url).pathname}"></script>`
+      })
+    }]
+  },
+  integrations: [tailwind()]
+});
--- server/app.js
+++ server/app.js
@@ -31,8 +31,40 @@ app.get("/api", (req, res) => {
     res.send("<h1>Bienvenido al servidor</h1>");
 });
 
-app.get("/api/products", (req, res) => {
-    res.send(products);
+/* OBTENER TODOS LOS PRODUCTOS O FILTRADOS POR CATEGORÍAS */
+
+app.get("/api/products", (req, res) => { //  /productos?categoria={nombre_categoria}
+    const category = req.query.category;
+
+    if(category) {
+        const productsCat = products.filter((item) => item.category == category);
+        res.send(productsCat);
+    } else {
+        res.send(products)
+    }
+    
+});
+
+/* OBTENER UN PRODUCTO POR SU ID */
+
+app.get("/api/products/:id", (req, res) => { //  /productos/{id}
+    const productId = req.params.id;
+    const product = products.filter((item) => item.id == productId);
+    res.send(product);
+});
+
+/* OBTENER PRODUCTOS DESTACADOS */
+
+app.get("/api/destacados", (req, res) => {
+    const destacados = products.filter((item) => {return item.destacados === true});
+    res.send(destacados);
+});
+
+/* OBTENER LAS CATEGORIAS */
+
+app.get("/api/categorias", (req, res) => {
+    const categorias = [...new Set(products.map(item => item.category))];
+    res.send(categorias);
 });
 
 app.listen(port, () => {
